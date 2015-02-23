ab1b3420e21d0127ea47086c94d98bdef07ae5b1
Entorick <entorick11@qq.com>
2015-02-23 21:33:56 +0800

    Init
    Init Commit

---
+++ src/com/baidu/chenchao05/senseword/SenseWordMap.java
@@ -0,0 +1,15 @@
+package com.baidu.chenchao05.senseword;
+
+/**
+ * 用HashMap存储敏感词Trie，用来匹配敏感词
+ * @author chenchao05
+ *
+ */
+public class SenseWordMap{
+    /**
+     * 构造函数
+     */
+    public SenseWordMap(){
+        
+    }
+}
---
+++ src/com/baidu/chenchao05/senseword/SenseWordTree.java
@@ -0,0 +1,10 @@
+package com.baidu.chenchao05.senseword;
+
+/**
+ * 敏感词Trie
+ * @author chenchao05
+ *
+ */
+public class SenseWordTree{
+
+}
